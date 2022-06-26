ec0bfcc56d2c20f73187e125465d03f01124a17e
Kusuhara <comvex.co.jp>
2022-06-26 12:16:25 +0900

    feat: :sparkles: add icestark settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
+import { getBasename } from "@ice/stark-app/lib";
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: "history",
+  base: "/",
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/vue2',
+      name: 'vue2-home',
+      component: HelloWorld
     }
   ]
 })
+
+// when microfrontend base url changed, always push as history
+router.push(getBasename());
+
+export default router
